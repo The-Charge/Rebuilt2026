10c01fa02d8b2a1cd807b44cfdd5c4dd646bc6a7
Marston Smith <marssmit29@midlandps.org>
2026-01-22 19:30:43 -0500

    added constant, periodic, stop, and object for motor

--- src/main/java/frc/robot/subsystems/IndexerSubsystem.java
+++ src/main/java/frc/robot/subsystems/IndexerSubsystem.java
@@ -3,13 +3,29 @@ package frc.robot.subsystems;
 import com.ctre.phoenix6.hardware.TalonFX;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.constants.IndexConstants;
 
 public class IndexerSubsystem extends SubsystemBase {
 
-    private TalonFX spindexerMotor;
+    private final TalonFX spindexerMotor;
     
     public IndexerSubsystem() {
         
+        spindexerMotor = new TalonFX(IndexConstants.spindexerMotorID);//port number under indexConstants
+
+        configureTalonFXMotor(); //always configure before their use
+    }
+@Override
+    public void periodic(){
+
+    }
+
+    public void stop(){
+        spindexerMotor.stopMotor();//safety
+    }
+
+    private void configureTalonFXMotor(){
+
     }
 }
 
