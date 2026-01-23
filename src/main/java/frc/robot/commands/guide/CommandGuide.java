81c14e58dc4af760f75c35d0b87f0761b0d6c238
Derek Riter <derekriter08@gmail.com>
2026-01-22 21:53:18 -0500

    Finish command guide

--- src/main/java/frc/robot/commands/guide/CommandGuide.java
+++ src/main/java/frc/robot/commands/guide/CommandGuide.java
@@ -8,6 +8,8 @@ import frc.robot.subsystems.SubsystemGuide;
 //ex. If you are making a command to spin up the indexer, then "SpinUp" is not a good name, as it doesn't make it clear that this command is for the indexer specifically. Something along the lines of "SpinUpIndexer" could be better.
 public class CommandGuide extends Command {
 
+    //this is just a reference to the subsystem that was passed in
+    //we need this so that we can use the subsystem in the other functions such as initialize and isFinished
     private final SubsystemGuide guideSub;
 
     public CommandGuide(SubsystemGuide _guideSub) {
@@ -17,22 +19,27 @@ public class CommandGuide extends Command {
 
     @Override
     public void initialize() {
+        //initialize runs only once, that being when the command starts
         guideSub.setExampleMotorVelocity(100);
     }
 
     @Override
     public void execute() {
         //execute is a lot like "periodic" for subsystems
-        //execute will run in a loop 
+        //execute will run in a loop until the command ends
     }
 
     @Override
     public void end(boolean interrupted) {
-
+        //end is called only once, when the command ends and is exiting
+        //the "interrupted" argument is true when the command was forcibly ended and is false when the command ended 'of its own will' so to speak
     }
 
     @Override
     public boolean isFinished() {
+        //isFinished determines when the command willing ends
+        //return true to end the command
+        //return false to keep the command running for another 'frame'
         return guideSub.getExampleMotorRPM() >= 90;
     }
 }
