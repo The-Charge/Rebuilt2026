077e95144fdde6ed0a927c35c543458403ed5421
Jake202558 <chuayang28@midlandps.org>
2026-01-22 19:44:18 -0500

    Create LEDSubsystem.java

---
+++ src/main/java/frc/robot/LEDSubsystem.java
@@ -0,0 +1,42 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import edu.wpi.first.wpilibj.util.Color;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class LEDSubsystem extends SubsystemBase {
+
+  AddressableLED led;  
+  AddressableLEDBuffer buffer;
+  public LEDSubsystem(){
+  led = new AddressableLED(9); //port number
+  buffer = new AddressableLEDBuffer(60); //nuumber of LEDs
+
+  led.setLength(buffer.getLength()); //set led buffer
+  led.setData(buffer);  
+  led.start();  
+}
+
+  @Override
+  public void periodic() {
+  update();
+  }
+
+  private void update() {
+    for(int i=0; i<buffer.getLength(); i++){
+      buffer.setLED(i, Color.kGreen); //LED color
+    }
+    led.setData(buffer);  //update to led
+  }
+
+  /**
+   * solid green LED
+   */
+  public void solidGreen(){
+    for(int i=0; i<buffer.getLength(); i++){
+      buffer.setLED(i, Color.kGreen);
+    }
+    led.setData(buffer);
+  }
+}
