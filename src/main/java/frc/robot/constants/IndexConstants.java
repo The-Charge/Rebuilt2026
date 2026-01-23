86356a4470e80ca6353cf25b77eef6457629b14c
Derek Riter <derekriter08@gmail.com>
2026-01-22 19:41:25 -0500

    Add proto configs for indexer

--- src/main/java/frc/robot/constants/IndexConstants.java
+++ src/main/java/frc/robot/constants/IndexConstants.java
@@ -1,6 +1,28 @@
 package frc.robot.constants;
 
+import java.util.Optional;
+
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
 public class IndexConstants {
 
     public static final int spindexerMotorID = 67;
+
+    public static final double maxCurrent = 30;
+    public static final NeutralModeValue neutralMode = NeutralModeValue.Coast;
+    public static final InvertedValue inverted = InvertedValue.Clockwise_Positive;
+    public static final double maxDutyCycle = 1;
+    public static final double kP = 0;
+    public static final double kI = 0;
+    public static final double kD = 0;
+    public static final double kG = 0;
+    //TODO: indexer units
+    public static final Optional<Double> forwardSoftLimit = Optional.empty();
+    public static final Optional<Double> reverseSoftLimit = Optional.empty();
+    public static final double maxVoltage = 16;
+    public static final boolean forwardHardLimitEnabled = false;
+    public static final Optional<Double> forwardHardLimitResetValue = Optional.empty();
+    public static final boolean reverseHardLimitEnabled = false;
+    public static final Optional<Double> reverseHardLimitResetValue = Optional.empty();
 }
--- src/main/java/frc/robot/subsystems/IndexerSubsystem.java
+++ src/main/java/frc/robot/subsystems/IndexerSubsystem.java
@@ -1,6 +1,10 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.StatusCode;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.GravityTypeValue;
+import com.ctre.phoenix6.signals.StaticFeedforwardSignValue;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.constants.IndexConstants;
@@ -25,7 +29,53 @@ public class IndexerSubsystem extends SubsystemBase {
     }
 
     private void configureTalonFXMotor(){
+        TalonFXConfiguration config = new TalonFXConfiguration();
 
+        // current limits
+        config.CurrentLimits.StatorCurrentLimit = IndexConstants.maxCurrent;
+        config.CurrentLimits.StatorCurrentLimitEnable = true;
+
+        // hardstops
+        config.HardwareLimitSwitch.ForwardLimitEnable = IndexConstants.forwardHardLimitEnabled;
+        config.HardwareLimitSwitch.ForwardLimitAutosetPositionEnable =
+                IndexConstants.forwardHardLimitResetValue.isPresent();
+        config.HardwareLimitSwitch.ForwardLimitAutosetPositionValue = IndexConstants.forwardHardLimitResetValue.orElse(0d);
+        config.HardwareLimitSwitch.ReverseLimitEnable = IndexConstants.reverseHardLimitEnabled;
+        config.HardwareLimitSwitch.ForwardLimitAutosetPositionEnable =
+                IndexConstants.reverseHardLimitResetValue.isPresent();
+        config.HardwareLimitSwitch.ForwardLimitAutosetPositionValue = IndexConstants.reverseHardLimitResetValue.orElse(0d);
+
+        // braking mode
+        config.MotorOutput.NeutralMode = IndexConstants.neutralMode;
+        // polarity
+        config.MotorOutput.Inverted = IndexConstants.inverted;
+        // duty cycle output limits
+        config.MotorOutput.PeakForwardDutyCycle = IndexConstants.maxDutyCycle;
+        config.MotorOutput.PeakReverseDutyCycle = -IndexConstants.maxDutyCycle;
+
+        // pid
+        config.Slot0.GravityType = GravityTypeValue.Elevator_Static;
+        config.Slot0.StaticFeedforwardSign = StaticFeedforwardSignValue.UseClosedLoopSign;
+        config.Slot0.kP = IndexConstants.kP;
+        config.Slot0.kI = IndexConstants.kI;
+        config.Slot0.kD = IndexConstants.kD;
+        config.Slot0.kG = IndexConstants.kG;
+
+        // softstops
+        config.SoftwareLimitSwitch.ForwardSoftLimitEnable = IndexConstants.forwardSoftLimit.isPresent();
+        config.SoftwareLimitSwitch.ForwardSoftLimitThreshold = IndexConstants.forwardSoftLimit.orElse(0d);
+        config.SoftwareLimitSwitch.ReverseSoftLimitEnable = IndexConstants.reverseSoftLimit.isPresent();
+        config.SoftwareLimitSwitch.ReverseSoftLimitThreshold = IndexConstants.reverseSoftLimit.orElse(0d);
+
+        // voltage output limits
+        config.Voltage.PeakForwardVoltage = IndexConstants.maxVoltage;
+        config.Voltage.PeakReverseVoltage = -IndexConstants.maxVoltage;
+
+        if (spindexerMotor.getConfigurator().apply(config) != StatusCode.OK) {
+            //TODO: error reporting
+            //Logger.reportError("Failed to configure elevator motor");
+            //Alerts.elevMotorConfigFail.set(true);
+        }
     }
 }
 
