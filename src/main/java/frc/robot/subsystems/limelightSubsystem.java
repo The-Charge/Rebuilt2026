80afa38ea54d258e74f3d1b653a4fcb28440ca71
James De Vries <dvjamest@gmail.com>
2026-01-24 10:55:09 -0500

    Create limelight subsystem

---
+++ src/main/java/frc/robot/subsystems/limelightSubsystem.java
@@ -0,0 +1,11 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class limelightSubsystem extends SubsystemBase {
+    private final String name;
+
+    public limelightSubsystem(String name) {
+        this.name = name;
+    }
+}
