db657607794e5ad8127e64e04e3b1f17177bc818
Vedaang Daga <vedadaga29@midlandps.org>
2026-01-24 13:50:40 -0500

    spin down made

---
+++ src/main/java/frc/robot/commands/Indexer/SpinDownIndexer.java
@@ -0,0 +1,27 @@
+package frc.robot.commands.Indexer;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.IndexerSubsystem;
+
+public class SpinDownIndexer extends Command {
+    public final IndexerSubsystem indexer;
+
+    public SpinDownIndexer(IndexerSubsystem indexerSub) {
+        indexer = indexerSub;
+        addRequirements(indexer);
+    }
+
+    @Override
+    public void initialize() {
+        indexer.setIndexerVelocity(0); // in this instance set velocity to ...
+    }
+
+    @Override
+    public boolean isFinished() {
+        if (indexer.getVelocityRPM() > 0) {
+            return false;
+        } else {
+            return true;
+        } // if velocity has reached or surpassed the set velocity then it will stop.
+    }
+}
