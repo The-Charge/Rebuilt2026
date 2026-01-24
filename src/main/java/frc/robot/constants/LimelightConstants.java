5317a8f1cf196c8b059b306ff16bb00ace10c43c
James De Vries <dvjamest@gmail.com>
2026-01-24 11:59:12 -0500

    I need swerve

---
+++ src/main/java/frc/robot/constants/LimelightConstants.java
@@ -0,0 +1,41 @@
+package frc.robot.constants;
+
+import static edu.wpi.first.units.Units.Seconds;
+
+import edu.wpi.first.units.measure.Time;
+
+public class LimelightConstants {
+    public static final double kMaxAngularSpeed = 720;
+    public static final double kMaxSpeedForMegaTag1 = 0.5; // meters
+    public static final double kMaxDistanceForMegaTag1 = 3.75; // meters
+    public static final double kMaxDistance = 8; // could be increased
+    public static final boolean kVisionDiagnostics = true;
+
+    public static final Time newMegaTag1ReadingThreshold = Seconds.of(10);
+
+    // public static final AprilTagFieldLayout kFieldLayout =
+    // AprilTagFieldLayout.loadField(AprilTagFields.k2025Reefscape);
+    public static final LimelightConstants kLimelights[] = {};
+
+    public static final class StdDevConstants {
+        public static final class MegaTag1 {
+            public static final double kInitialValue = 0.3;
+            public static final double kTagCountReward = 0.15;
+            public static final double kAverageDistancePunishment = 0.1;
+            public static final double kRobotSpeedPunishment = 0.15;
+            public static final double kSingleTagPunishment = 0.3;
+        }
+
+        public static final class MegaTag2 {
+            public static final double kInitialValue = 0.2;
+            public static final double kAverageDistancePunishment = 0.075;
+            public static final double kRobotSpeedPunishment = 0.25;
+            public static final double kMultipleTagsBonus = 0.05;
+        }
+    }
+
+    public enum PoseEstimationMethod {
+        MEGATAG_1,
+        MEGATAG_2
+    }
+}
--- src/main/java/frc/robot/subsystems/limelightSubsystem.java
+++ src/main/java/frc/robot/subsystems/limelightSubsystem.java
@@ -1,11 +1,35 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.LimelightHelpers;
+import frc.robot.LimelightHelpers.PoseEstimate;
+import frc.robot.constants.LimelightConstants;
+import java.util.Optional;
 
 public class LimelightSubsystem extends SubsystemBase {
     private final String name;
+    // CommandSwerveDrivetrain drivetrain;
 
     public LimelightSubsystem(String name) {
-        this.name = name;
+        this.name = "limelight-" + name;
     }
+
+    public Optional<PoseEstimate> getVisionMeasurement() {
+        boolean useMegaTag2 = true;
+        final PoseEstimate poseEstimate = LimelightHelpers.getBotPoseEstimate_wpiBlue(name);
+        if (!LimelightHelpers.validPoseEstimate(poseEstimate)) return Optional.empty();
+        if (poseEstimate.avgTagDist > LimelightConstants.kMaxDistance) return Optional.empty();
+        final boolean twoOrMoreTags = poseEstimate.tagCount >= 2;
+        final boolean closeEnough = poseEstimate.avgTagDist < LimelightConstants.kMaxDistanceForMegaTag1;
+        final double robotSpeed = 0; // get swerve speed from ctre
+        final boolean movingSlowEnough = robotSpeed < LimelightConstants.kMaxSpeedForMegaTag1;
+        final boolean CAN_GET_GOOD_HEADING = twoOrMoreTags && movingSlowEnough && closeEnough;
+        if (!CAN_GET_GOOD_HEADING) return Optional.empty();
+        if (CAN_GET_GOOD_HEADING) useMegaTag2 = false;
+        return Optional.empty();
+    }
+
+    // public Optional<PoseEstimate> getVisionMeasurement(SwerveSubsystem swerve, boolean useMegaTag2) {
+    //     LimelightHelpers.SetRobotOrientation(name, 0, 0, 0, 0, 0, 0);
+    // }
 }
