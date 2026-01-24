999b07fc20c00ee21cd8f6aef9ccd9ab1b0285e8
Marcos Grzesiak <marcosgrzesiak@gmail.com>
2026-01-24 11:29:12 -0500

    bind turret command to controller

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,15 +6,30 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.commands.pointAndShoot;
+import frc.robot.subsystems.TurretSubsystem;
 
 public class RobotContainer {
-  public RobotContainer() {
-    configureBindings();
-  }
+    private final CommandXboxController driver1;
+    private final CommandXboxController driver2;
 
-  private void configureBindings() {}
+    public TurretSubsystem turretSystem;
 
-  public Command getAutonomousCommand() {
-    return Commands.print("No autonomous command configured");
-  }
+    public RobotContainer() {
+        configureBindings();
+
+        turretSystem = new TurretSubsystem();
+
+        driver1 = new CommandXboxController(0);
+        driver2 = new CommandXboxController(1);
+    }
+
+    private void configureBindings() {
+        driver2.x().onTrue(new pointAndShoot(turretSystem));
+    }
+
+    public Command getAutonomousCommand() {
+        return Commands.print("No autonomous command configured");
+    }
 }
