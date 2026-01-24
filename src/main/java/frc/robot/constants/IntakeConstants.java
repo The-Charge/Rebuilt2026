e7211581328de69f34a0a986fbbe779c4a2c9265
Marcos Grzesiak <marcosgrzesiak@gmail.com>
2026-01-24 10:15:25 -0500

    add values for pivot motor config based on reefscape elevator

--- src/main/java/frc/robot/constants/IntakeConstants.java
+++ src/main/java/frc/robot/constants/IntakeConstants.java
@@ -4,7 +4,6 @@ import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 
 public class IntakeConstants {
-    public static final double maxVBus = 0;
     public static int RollerMotorId = 0;
     public static int PivotMotorId = 0;
 
@@ -20,18 +19,18 @@ public class IntakeConstants {
     public static double RollerVelocity = 123.456;
 
     public static double StateTolerance = 1.23456;
-    public static NeutralModeValue neutralMode;
-    public static InvertedValue inverted;
-    public static double maxCurrent;
-    public static double maxPosTicks;
-    public static double minPosTicks;
+
+    public static double maxVBus = 1.00;
+    public static NeutralModeValue neutralMode = NeutralModeValue.Brake;
+    public static InvertedValue inverted = InvertedValue.Clockwise_Positive;
+    public static double maxCurrent = 35;
+    public static double minPosTicks = 0;
+    public static double maxPosTicks = 123;
 
     public static class pidf {
-
-        public static double p;
-        public static double i;
-        public static double d;
-        public static double f;
+        public static double p = 0.3;
+        public static double i = 0;
+        public static double d = 0.02;
+        public static double f = 0;
     }
-    ;
 }
