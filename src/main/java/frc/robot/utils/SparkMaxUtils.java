6301fc7b1f5533c9920819ec5843fff01234c665
Derek Riter <derekriter08@gmail.com>
2026-01-28 16:22:41 -0500

    Add motor configuration functions

--- src/main/java/frc/robot/utils/SparkMaxUtils.java
+++ src/main/java/frc/robot/utils/SparkMaxUtils.java
@@ -3,6 +3,10 @@ package frc.robot.utils;
 import com.revrobotics.spark.SparkBase.Faults;
 import com.revrobotics.spark.SparkBase.Warnings;
 import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.LimitSwitchConfig.Behavior;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import java.util.Optional;
 
 public class SparkMaxUtils {
 
@@ -39,5 +43,125 @@ public class SparkMaxUtils {
         }
 
         return motor.getFirmwareVersion() != 0;
+    }
+
+    public void configureBasicSettings(
+            SparkMaxConfig config,
+            int maxCurrent,
+            IdleMode idleMode,
+            boolean inverted,
+            double maxDutyCycle,
+            Optional<Double> voltageCompensation) {
+        if (config == null) {
+            Logger.reportWarning("Cannot modify a null SparkMaxConfig", true);
+            return;
+        }
+
+        config.smartCurrentLimit(maxCurrent);
+        config.idleMode(idleMode);
+        config.inverted(inverted);
+        config.closedLoop.outputRange(-maxDutyCycle, maxDutyCycle);
+
+        if (voltageCompensation != null && voltageCompensation.isPresent()) {
+            config.voltageCompensation(voltageCompensation.get());
+        } else {
+            config.disableVoltageCompensation();
+        }
+    }
+
+    /**
+     * @param config
+     * @param kP
+     * @param kI
+     * @param kD
+     * @param kStaticG this parameter should be {@code Optional.empty()} if {@code kCos} is not zero
+     * @param kCos this parameter should be {@code Optional.empty()} if {@code kStaticG} is not zero
+     * @param kS
+     * @param kV
+     * @param kA
+     */
+    public void configureClosedLoopSettings(
+            SparkMaxConfig config,
+            double kP,
+            double kI,
+            double kD,
+            Optional<Double> kStaticG,
+            Optional<Double> kCos,
+            Optional<Double> kS,
+            Optional<Double> kV,
+            Optional<Double> kA) {
+        if (config == null) {
+            Logger.reportWarning("Cannot modify a null SparkMaxConfig", true);
+            return;
+        }
+
+        config.closedLoop.pid(kP, kI, kD);
+        if (kStaticG != null && kStaticG.isPresent()) {
+            config.closedLoop.feedForward.kG(kStaticG.get());
+        }
+        if (kCos != null && kCos.isPresent()) {
+            config.closedLoop.feedForward.kCos(kCos.get());
+        }
+        if (kS != null && kS.isPresent()) {
+            config.closedLoop.feedForward.kS(kS.get());
+        }
+        if (kV != null && kV.isPresent()) {
+            config.closedLoop.feedForward.kV(kV.get());
+        }
+        if (kA != null && kA.isPresent()) {
+            config.closedLoop.feedForward.kA(kA.get());
+        }
+    }
+
+    public void configureSoftStops(
+            SparkMaxConfig config, Optional<Double> forwardLimit, Optional<Double> reverseLimit) {
+        if (config == null) {
+            Logger.reportWarning("Cannot modify a null SparkMaxConfig", true);
+            return;
+        }
+
+        if (forwardLimit != null) {
+            config.softLimit.forwardSoftLimitEnabled(forwardLimit.isPresent());
+            config.softLimit.forwardSoftLimit(forwardLimit.orElse(0d));
+        }
+
+        if (reverseLimit != null) {
+            config.softLimit.reverseSoftLimitEnabled(reverseLimit.isPresent());
+            config.softLimit.reverseSoftLimit(reverseLimit.orElse(0d));
+        }
+    }
+
+    public void configureHardStops(
+            SparkMaxConfig config,
+            boolean forwardLimitEnabled,
+            Optional<Double> forwardLimitResetValue,
+            boolean reverseLimitEnabled,
+            Optional<Double> reverseLimitResetValue) {
+        if (config == null) {
+            Logger.reportWarning("Cannot modify a null SparkMaxConfig", true);
+            return;
+        }
+
+        if (forwardLimitEnabled) {
+            if (forwardLimitResetValue != null && forwardLimitResetValue.isPresent()) {
+                config.limitSwitch.forwardLimitSwitchTriggerBehavior(Behavior.kStopMovingMotorAndSetPosition);
+                config.limitSwitch.forwardLimitSwitchPosition(forwardLimitResetValue.get());
+            } else {
+                config.limitSwitch.forwardLimitSwitchTriggerBehavior(Behavior.kStopMovingMotor);
+            }
+        } else {
+            config.limitSwitch.forwardLimitSwitchTriggerBehavior(Behavior.kKeepMovingMotor);
+        }
+
+        if (reverseLimitEnabled) {
+            if (reverseLimitResetValue != null && reverseLimitResetValue.isPresent()) {
+                config.limitSwitch.reverseLimitSwitchTriggerBehavior(Behavior.kStopMovingMotorAndSetPosition);
+                config.limitSwitch.reverseLimitSwitchPosition(reverseLimitResetValue.get());
+            } else {
+                config.limitSwitch.reverseLimitSwitchTriggerBehavior(Behavior.kStopMovingMotor);
+            }
+        } else {
+            config.limitSwitch.reverseLimitSwitchTriggerBehavior(Behavior.kKeepMovingMotor);
+        }
     }
 }
--- src/main/java/frc/robot/utils/TalonFXUtils.java
+++ src/main/java/frc/robot/utils/TalonFXUtils.java
@@ -1,6 +1,12 @@
 package frc.robot.utils;
 
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.GravityTypeValue;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+import com.ctre.phoenix6.signals.StaticFeedforwardSignValue;
+import java.util.Optional;
 
 public class TalonFXUtils {
 
@@ -74,5 +80,114 @@ public class TalonFXUtils {
                 motor.getStickyFault_UnlicensedFeatureInUse().getValue(),
                 motor.getStickyFault_UnstableSupplyV().getValue(),
                 motor.getStickyFault_UsingFusedCANcoderWhileUnlicensed().getValue());
+    }
+
+    public static void configureBasicSettings(
+            TalonFXConfiguration config,
+            double maxCurrent,
+            NeutralModeValue neutralMode,
+            InvertedValue inverted,
+            double maxDutyCycle,
+            Optional<Double> maxVoltage) {
+        if (config == null) {
+            Logger.reportWarning("Cannot modify a null TalonFXConfiguration", true);
+            return;
+        }
+
+        config.CurrentLimits.StatorCurrentLimit = maxCurrent;
+        config.CurrentLimits.StatorCurrentLimitEnable = true;
+
+        config.MotorOutput.NeutralMode = neutralMode;
+
+        config.MotorOutput.Inverted = inverted;
+
+        config.MotorOutput.PeakForwardDutyCycle = maxDutyCycle;
+        config.MotorOutput.PeakReverseDutyCycle = -maxDutyCycle;
+
+        if (maxVoltage != null && maxVoltage.isPresent()) {
+            config.Voltage.PeakForwardVoltage = maxVoltage.get();
+            config.Voltage.PeakReverseVoltage = -maxVoltage.get();
+        }
+    }
+
+    public static void configureClosedLoopSettings(
+            TalonFXConfiguration config,
+            double kP,
+            double kI,
+            double kD,
+            Optional<Double> kG,
+            Optional<GravityTypeValue> gravityType,
+            Optional<Double> kS,
+            Optional<StaticFeedforwardSignValue> kSSign,
+            Optional<Double> kV,
+            Optional<Double> kA) {
+        if (config == null) {
+            Logger.reportWarning("Cannot modify a null TalonFXConfiguration", true);
+            return;
+        }
+
+        config.Slot0.kP = kP;
+        config.Slot0.kI = kI;
+        config.Slot0.kD = kD;
+        if (kG != null && kG.isPresent()) {
+            config.Slot0.kG = kG.get();
+        }
+        if (gravityType != null && gravityType.isPresent()) {
+            config.Slot0.GravityType = gravityType.get();
+        }
+        if (kS != null && kS.isPresent()) {
+            config.Slot0.kS = kS.get();
+        }
+        if (kSSign != null && kSSign.isPresent()) {
+            config.Slot0.StaticFeedforwardSign = kSSign.get();
+        }
+        if (kV != null && kV.isPresent()) {
+            config.Slot0.kV = kV.get();
+        }
+        if (kA != null && kA.isPresent()) {
+            config.Slot0.kA = kA.get();
+        }
+    }
+
+    public static void configureSoftStops(
+            TalonFXConfiguration config, Optional<Double> forwardLimit, Optional<Double> reverseLimit) {
+        if (config == null) {
+            Logger.reportWarning("Cannot modify a null TalonFXConfiguration", true);
+            return;
+        }
+
+        if (forwardLimit != null) {
+            config.SoftwareLimitSwitch.ForwardSoftLimitEnable = forwardLimit.isPresent();
+            config.SoftwareLimitSwitch.ForwardSoftLimitThreshold = forwardLimit.orElse(0d);
+        }
+
+        if (reverseLimit != null) {
+            config.SoftwareLimitSwitch.ReverseSoftLimitEnable = reverseLimit.isPresent();
+            config.SoftwareLimitSwitch.ReverseSoftLimitThreshold = reverseLimit.orElse(0d);
+        }
+    }
+
+    public static void configureHardStops(
+            TalonFXConfiguration config,
+            boolean forwardLimitEnabled,
+            Optional<Double> forwardLimitResetValue,
+            boolean reverseLimitEnabled,
+            Optional<Double> reverseLimitResetValue) {
+        if (config == null) {
+            Logger.reportWarning("Cannot modify a null TalonFXConfiguration", true);
+            return;
+        }
+
+        config.HardwareLimitSwitch.ForwardLimitEnable = forwardLimitEnabled;
+        if (forwardLimitResetValue != null) {
+            config.HardwareLimitSwitch.ForwardLimitAutosetPositionEnable = forwardLimitResetValue.isPresent();
+            config.HardwareLimitSwitch.ForwardLimitAutosetPositionValue = forwardLimitResetValue.orElse(0d);
+        }
+
+        config.HardwareLimitSwitch.ReverseLimitEnable = reverseLimitEnabled;
+        if (reverseLimitResetValue != null) {
+            config.HardwareLimitSwitch.ReverseLimitAutosetPositionEnable = reverseLimitResetValue.isPresent();
+            config.HardwareLimitSwitch.ReverseLimitAutosetPositionValue = reverseLimitResetValue.orElse(0d);
+        }
     }
 }
