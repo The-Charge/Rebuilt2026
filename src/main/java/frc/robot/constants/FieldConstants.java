2119928bbd5b64b6142802a3b90cd115384e140e
James De Vries <dvjamest@gmail.com>
2026-01-27 19:13:02 -0500

    use the numbers from manual in inches

--- src/main/java/frc/robot/constants/FieldConstants.java
+++ src/main/java/frc/robot/constants/FieldConstants.java
@@ -1,10 +1,15 @@
 package frc.robot.constants;
 
+import static edu.wpi.first.units.Units.Inches;
+import static edu.wpi.first.units.Units.Meters;
+
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 
 public class FieldConstants {
-    public static final Pose2d redHubPos = new Pose2d(11.92, 4.03, new Rotation2d());
-    public static final Pose2d blueHubPos = new Pose2d(4.45, 4.03, new Rotation2d());
+    public static final Pose2d redHubPos = new Pose2d(
+            Meters.convertFrom(651.22 - 182.11, Inches), Meters.convertFrom(158.84, Inches), new Rotation2d());
+    public static final Pose2d blueHubPos =
+            new Pose2d(Meters.convertFrom(182.11, Inches), Meters.convertFrom(158.84, Inches), new Rotation2d());
     public static final double hubHeight = -1;
 }
