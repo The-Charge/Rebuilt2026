410191aa8088a68e1764e9d0c27a3332a059389a
Marcos Grzesiak <marcosgrzesiak@gmail.com>
2026-01-27 20:04:32 -0500

    align turret by default

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,10 +6,21 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.commands.vision.AlignTurret;
+import frc.robot.subsystems.LimelightSub;
+import frc.robot.subsystems.TurretSubsystem;
 
 public class RobotContainer {
+    public TurretSubsystem turret;
+    public LimelightSub turretLimelight;
+
     public RobotContainer() {
         configureBindings();
+
+        turret = new TurretSubsystem();
+        turretLimelight = new LimelightSub("turret");
+
+        turret.setDefaultCommand(new AlignTurret(turretLimelight, turret));
     }
 
     private void configureBindings() {}
