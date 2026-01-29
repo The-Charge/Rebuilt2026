5d5ab781e494618527630232c4a2f926a7607e97
Marcos Grzesiak <marcosgrzesiak@gmail.com>
2026-01-29 18:58:20 -0500

    remove superfluous imports and variables giving warning

--- src/main/java/frc/robot/ClimbCommand.java
+++ src/main/java/frc/robot/ClimbCommand.java
@@ -1,16 +1,10 @@
 package frc.robot;
 
-import com.ctre.phoenix6.controls.PositionDutyCycle;
-
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.constants.ClimberConstants;
-import frc.robot.subsystems.SubsystemGuide;
 
 public class ClimbCommand extends Command {
     private final ClimbSubsystem climbSubsystem;
-
 
     public ClimbCommand(ClimbSubsystem climb) {
         climbSubsystem = climb;
@@ -19,30 +13,16 @@ public class ClimbCommand extends Command {
 
     @Override
     public void initialize() {
-        // initialize runs only once, that being when the command starts
-         
         climbSubsystem.setClimbMotorPosition(ClimberConstants.Position);
-
     }
 
-
-        @Override
+    @Override
     public void end(boolean interrupted) {
-        // end is called only once, when the command ends and is exiting
-        // the "interrupted" argument is true when the command was forcibly ended and is false when the command ended
-        //     'of its own will'
-       
-        climbSubsystem.stop();        
+        climbSubsystem.stop();
     }
 
     @Override
     public boolean isFinished() {
-        // isFinished determines when the command willing ends
-        // return true to end the command
-        // return false to keep the command running for another 'frame'
-         final double robotPos;
-
-        robotPos = climbSubsystem.getPosition();
         return climbSubsystem.getPosition() >= 90;
     }
 }
--- src/main/java/frc/robot/ClimbSubsystem.java
+++ src/main/java/frc/robot/ClimbSubsystem.java
@@ -1,32 +1,25 @@
 package frc.robot;
 
-import com.ctre.phoenix6.controls.DutyCycleOut;
 import com.ctre.phoenix6.controls.PositionVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
-
 import edu.wpi.first.units.Units;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class ClimbSubsystem extends SubsystemBase{
+public class ClimbSubsystem extends SubsystemBase {
     private final TalonFX climber;
 
-
-    public ClimbSubsystem (){
+    public ClimbSubsystem() {
         climber = new TalonFX(0);
         configureTalonFXMotor();
-
-
     }
 
-        @Override
-    public void periodic() {
+    @Override
+    public void periodic() {}
 
-    }
-
-     // example code to set the closed-loop (PID) target velocity to a given value
+    // example code to set the closed-loop (PID) target velocity to a given value
     // this function is public so that it can be called from commands using this subsystem
     public void setClimbMotorPosition(double Position) {
-        PositionVoltage  request = new PositionVoltage(Position);
+        PositionVoltage request = new PositionVoltage(Position);
         climber.setControl(request);
     }
 
@@ -35,10 +28,8 @@ public class ClimbSubsystem extends SubsystemBase{
     // this would usually be called in Robot.disabledInit
     public void stop() {
         climber.stopMotor();
-      
     }
 
-    // example code to get the current velocity of a motor
     public double getPosition() {
         return climber.getPosition().getValue().abs(Units.Radian);
     }
@@ -50,7 +41,4 @@ public class ClimbSubsystem extends SubsystemBase{
         //     it is being programmed for
         // thus, I have left this function blank. Just ask somebody experienced for help once you get here
     }
-
-
-    
 }
