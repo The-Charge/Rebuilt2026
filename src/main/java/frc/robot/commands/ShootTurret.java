c518691bdcf08bac2249cf210eb3458039ed3004
Marcos Grzesiak <marcosgrzesiak@gmail.com>
2026-02-07 12:47:49 -0500

    move interpolator into constants

--- src/main/java/frc/robot/commands/ShootTurret.java
+++ src/main/java/frc/robot/commands/ShootTurret.java
@@ -2,22 +2,14 @@ package frc.robot.commands;
 
 import static edu.wpi.first.units.Units.RPM;
 
-import edu.wpi.first.math.interpolation.InterpolatingDoubleTreeMap;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.constants.ShooterConstants;
 import frc.robot.subsystems.ShooterSubsystem;
 import frc.robot.subsystems.TurretSubsystem;
 
 public class ShootTurret extends Command {
     private TurretSubsystem turretSub;
     private ShooterSubsystem shooterSub;
-
-    private static final InterpolatingDoubleTreeMap distanceToRPMPlot;
-
-    static {
-        distanceToRPMPlot = new InterpolatingDoubleTreeMap();
-        distanceToRPMPlot.put(0.0, 0.0);
-        distanceToRPMPlot.put(99.0, 99.0);
-    }
 
     public ShootTurret(TurretSubsystem turretSubsystem, ShooterSubsystem shooterSubsystem) {
         this.turretSub = turretSubsystem;
@@ -34,7 +26,7 @@ public class ShootTurret extends Command {
     public void execute() {
         double distance = -1; // TODO: Get distance from limelights
 
-        double predictedRPM = distanceToRPMPlot.get(distance); // get power from distance
+        double predictedRPM = ShooterConstants.distanceToRPMPlot.get(distance); // get power from distance
 
         shooterSub.shoot(RPM.of(predictedRPM));
     }
--- src/main/java/frc/robot/constants/ShooterConstants.java
+++ src/main/java/frc/robot/constants/ShooterConstants.java
@@ -2,6 +2,7 @@ package frc.robot.constants;
 
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.interpolation.InterpolatingDoubleTreeMap;
 
 public class ShooterConstants { // split into shooter, spinner, and hood
 
@@ -32,5 +33,13 @@ public class ShooterConstants { // split into shooter, spinner, and hood
         public static IdleMode idleMode = IdleMode.kBrake;
 
         public static double ticksPerRadian;
+    }
+
+    public static final InterpolatingDoubleTreeMap distanceToRPMPlot;
+
+    static {
+        distanceToRPMPlot = new InterpolatingDoubleTreeMap();
+        distanceToRPMPlot.put(0.0, 0.0);
+        distanceToRPMPlot.put(99.0, 99.0);
     }
 }
