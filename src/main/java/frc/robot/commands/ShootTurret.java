0e77feadc13bab35c1540f3d40987008dc90a6c8
Marcos Grzesiak <marcosgrzesiak@gmail.com>
2026-02-07 10:55:09 -0500

    remove vibecoded vision

--- src/main/java/frc/robot/commands/ShootTurret.java
+++ src/main/java/frc/robot/commands/ShootTurret.java
@@ -1,37 +1,22 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.subsystems.ShooterSubsystem;
 import frc.robot.subsystems.TurretSubsystem;
-import frc.robot.subsystems.ShooterSubsystem.HoodPos;
 
 public class ShootTurret extends Command {
-    public ShooterSubsystem shootSub;
+    public TurretSubsystem turretSubsystem;
 
-    public ShooterSubsystem.HoodPos hoodPos;
-    
-        private VisionSubsystem vSub;
-    
-        public ShootTurret(ShooterSubsystem shootSub, VisionSubsystem vSub) {
-            this.shootSub = shootSub;
-            this.vSub = vSub;
+    public ShootTurret(TurretSubsystem turretSubsystem) {
+        this.turretSubsystem = turretSubsystem;
 
-        addRequirements(shootSub);
+        addRequirements(turretSubsystem);
     }
 
     @Override
-    public void initialize() {
-    }
+    public void initialize() {}
 
     @Override
-    public void execute() {
-        if (vSub.getTransformToTag().magnitude > ShooterSubsystem.hoodPosThreshold) { // change the way we get distance ?
-            shootSub.setHoodPos(HoodPos.DOWN); // if close go high
-        } else {
-            shootSub.setHoodPos(HoodPos.UP); // if far go far
-        }
-        shootSub.shoot();
-    }
+    public void execute() {}
 
     @Override
     public void end(boolean interrupted) {}
