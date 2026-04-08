b5be1ba3c228d4c06cdca573c1fef6a902522254
James De Vries <dvjamest@gmail.com>
2026-04-07 20:26:51 -0400

    Integer Division and reversion

--- src/main/java/frc/robot/constants/IndexerConstants.java
+++ src/main/java/frc/robot/constants/IndexerConstants.java
@@ -2,6 +2,8 @@ package frc.robot.constants;
 
 import static edu.wpi.first.units.Units.Amps;
 
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import edu.wpi.first.units.measure.Current;
 import edu.wpi.first.units.measure.Voltage;
@@ -16,15 +18,15 @@ public class IndexerConstants {
         public static final String motorName = "spindexerMotor";
 
         public static final Current maxCurrent = Amps.of(40);
-        public static final IdleMode idleMode = IdleMode.kCoast;
-        public static final boolean inverted = true;
+        public static final NeutralModeValue neutralMode = NeutralModeValue.Coast;
+        public static final InvertedValue inverted = InvertedValue.Clockwise_Positive;
         public static final double maxDutyCycle = 1;
         public static final Optional<Voltage> maxVoltage = Optional.empty();
 
         public static final double shootVoltage = 12;
         // public static final double reverseVoltage = -1 * 9 / 16.0 * 1.5;
-        public static final double slowReverseVoltage = -1.5 * (16 / 27);
-        public static final double fastReverseVoltage = -12 * 0.5 * (16 / 27);
+        public static final double slowReverseVoltage = -1.5 * (16. / 27);
+        public static final double fastReverseVoltage = -12 * 0.5 * (16. / 27);
     }
 
     public static class Exchange {
